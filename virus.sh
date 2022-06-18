407ebee21f72c5d98090ced3485899d8a8dbb9d4
IHannes <hannes.hildner@icloud.com>
2022-06-18 11:33:49 +0000

    Gag virus with zenity gui

---
+++ virus.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+#THIS IS A GAG - DOES NOTHING BUT YOU CAN MAKE SB BELIEVE HIS COMPUTER IS INFECTED WITH A VIRUS
+
+NAME=$(whoami)
+
+zenity --warning --title="VIRUS" --text="HALLO ${NAME^^} DAS IST EIN VIRUS - IHR COMPUTER WURDE GEHACKT" --no-wrap
